cb2c9356fcf3d7171a3522c6db6c5f6565c7b361
Ivan Song <mousewurx@gmail.com>
2024-07-24 19:53:42 -0700

    first commit

---
+++ quote/display.js
@@ -0,0 +1,58 @@
+function ShowToday(OBJ) {
+if (OBJ == "1") {
+t1.style.display = "";
+t2.style.display = "none";
+}
+if (OBJ == "2") {
+t1.style.display = "none";
+t2.style.display = "";
+}
+
+if (OBJ == "4") {
+c4.style.display = "";
+c4a.style.display = "none";
+c5.style.display = "none";
+c5a.style.display = "none";
+c6.style.display = "none";
+c7.style.display = "none";
+}
+if (OBJ == "4a") {
+c4.style.display = "none";
+c4a.style.display = "";
+c5.style.display = "none";
+c5a.style.display = "none";
+c6.style.display = "none";
+c7.style.display = "none";
+}
+if (OBJ == "5") {
+c4.style.display = "none";
+c4a.style.display = "none";
+c5.style.display = "";
+c5a.style.display = "none";
+c6.style.display = "none";
+c7.style.display = "none";
+}
+if (OBJ == "5a") {
+c4.style.display = "none";
+c4a.style.display = "none";
+c5.style.display = "none";
+c5a.style.display = "";
+c6.style.display = "none";
+c7.style.display = "none";
+}
+if (OBJ == "6") {
+c4.style.display = "none";
+c4a.style.display = "none";
+c5.style.display = "none";
+c5a.style.display = "none";
+c6.style.display = "";
+c7.style.display = "none";
+}
+if (OBJ == "7") {
+c4.style.display = "none";
+c4a.style.display = "none";
+c5.style.display = "none";
+c5a.style.display = "none";
+c6.style.display = "none";
+c7.style.display = "";
+}}
---
+++ services/display.js
@@ -0,0 +1,25 @@
+function ShowToday(OBJ) {
+if (OBJ == "1") {
+t1.style.display = "";
+t1a.style.display = "";
+t2.style.display = "none";
+t2a.style.display = "none";
+t3.style.display = "none";
+t3a.style.display = "none";
+}
+if (OBJ == "2") {
+t1.style.display = "none";
+t1a.style.display = "none";
+t2.style.display = "";
+t2a.style.display = "";
+t3.style.display = "none";
+t3a.style.display = "none";
+}
+if (OBJ == "3") {
+t1.style.display = "none";
+t1a.style.display = "none";
+t2.style.display = "none";
+t2a.style.display = "none";
+t3.style.display = "";
+t3a.style.display = "";
+}}
